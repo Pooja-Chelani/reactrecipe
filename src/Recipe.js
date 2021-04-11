f2890d2be9c8cc8b3ca82c97b6a7961d2bbcf16a
Pooja-Chelani <poojachelani@gmail.com>
2021-04-11 19:52:48 +0530

    my recipe commit website

---
+++ src/Recipe.js
@@ -0,0 +1,16 @@
+import React from "react";
+const Recipe=({title,calories,image,ingredients})=>{
+    return(
+       <div className="Recipe">
+           <h1>{title}</h1>
+           <ol>
+               {ingredients.map(ingredient=>(
+                 <li>{ingredient.text}</li>
+               ))}
+           </ol>
+           <p>{calories}</p>
+           <img className="image" src={image} alt =""/>
+       </div>
+    );
+};
+export default Recipe;
